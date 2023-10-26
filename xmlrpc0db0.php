1fb7c383c050e62826bc8fff1f1be8e5af7a42b8
Raghu Raja <raghunath88@gmail.com>
2023-10-26 00:25:26 -0700

    Initial commit
    Signed-off-by: Raghu Raja <raghunath88@gmail.com>

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://veecons.in/nichetemp</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://veecons.in/nichetemp/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://veecons.in/nichetemp/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://veecons.in/nichetemp/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://veecons.in/nichetemp/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://veecons.in/nichetemp/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
